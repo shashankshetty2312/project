899d102fdf00869b5a6939b960e88bf34c630817
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-03 21:26:56 +0530

    Update response_parser.py

--- aiotorrent/core/response_parser.py
+++ aiotorrent/core/response_parser.py
@@ -2,125 +2,104 @@ import logging
 from struct import unpack
 from struct import error as UnpackError
 
-
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-
 class PeerResponseParser:
-	def __init__(self, response):
-		self.response = response
-		
-		self.messages = {
-		# self created index. Does not necessarily follow bittorrent protocol specification
-			0: self.parse_choke,
-			1: self.parse_unchoke,
-			4: self.parse_have,
-			5: self.parse_bitfield,
-			7: self.parse_piece,
-			19: self.parse_handshake,
-			None: self.parse_keep_alive
-		}
-		self.artifacts = dict()
+    def __init__(self, response):
+        self.response = response
+        self.messages = {
+            0: self.parse_choke,
+            1: self.parse_unchoke,
+            4: self.parse_have,
+            5: self.parse_bitfield,
+            7: self.parse_piece,
+            19: self.parse_handshake,
+            None: self.parse_keep_alive
+        }
+        self.artifacts = dict()
 
+    def parse(self):
+        while self.response:
+            try:
+                # Technical Fix: Check length before accessing index to avoid IndexError
+                if len(self.response) >= 1 and self.response[0] == 19:
+                    self.parse_handshake()
+                    continue
 
-	def parse(self):
+                if len(self.response) < 4:
+                    break
 
-		while self.response:
-			try:
-				# if first byte of response is 19 bytes, it's a handshake
-				if self.response[0] == 19:
-					self.parse_handshake()
-					continue #	issue #1
+                self.message_len = unpack('>I', self.response[:4])[0]
+                
+                # Security: Prevent OOM attacks by capping maximum message length
+                if self.message_len > 1048576: # 1MB limit for safety
+                    logger.error("Message length exceeds safety threshold")
+                    self.response = bytes()
+                    break
 
-				# message len and id need to be class wide accessible because message len
-				# needs to be updated in case of a bitfield and piece requests
-				self.message_len = unpack('>I', self.response[:4])[0]
-				self.message_id = unpack('>B', self.response[4:5])[0] if self.message_len != 0 else None
-					
-				# keep-alive messages have length 0 and no message_id
-				if self.message_len == 0 and not self.message_id: self.parse_keep_alive()
+                self.message_id = unpack('>B', self.response[4:5])[0] if self.message_len != 0 else None
+                
+                if self.message_len == 0:
+                    self.parse_keep_alive()
+                    continue
 
-				# if msg id not in message index, clear response
-				if self.message_id not in self.messages:
-					logger.warning(f"{self.message_id=}, {self.message_len=}, {self.response[:16]}")
-					self.response = bytes()
+                if self.message_id not in self.messages:
+                    self.response = bytes()
+                    break
 
-				# finally parse the blob of response
-				logger.debug(f"{self.message_len=}, {self.message_id=}, {self.response[:16]=}")
-				self.messages[self.message_id]()
+                self.messages[self.message_id]()
 
-			# In case of general exception, clear the response
-			except Exception as E:
-				logger.warning(f"Parser: {E}")
-				self.response = bytes()
-			
-		return self.artifacts
+            except Exception as e:
+                logger.warning(f"Parser error: {e}")
+                self.response = bytes()
+            
+        return self.artifacts
 
-		
-	def parse_keep_alive(self):
-		# keep-alive
-		message = self.response[:4]
-		self.response = self.response[4:]
-		self.artifacts.update({'keep_alive': True})
-		
-	
-	def parse_choke(self):
-		# client got choked by peer
-		message = self.response[:5]
-		self.response = self.response[5:]
-		self.artifacts.update({'choke': True})
-		
-		
-	def parse_unchoke(self):
-		message = self.response[:5]
-		self.response = self.response[5:]
-		self.artifacts.update({'unchoke': True})
-		
-		
-	def parse_have(self):
-		message = self.response[:9]
-		# We just need the piece index so we can ignore the first 5 bytes of message.
-		# Unpack returns tuple , selecting first element.
-		piece_index = unpack('>I', message[5:])[0]
-		self.response = self.response[9:]
-		self.artifacts.update({'have': {piece_index: True}})
-		
-	
-	def parse_piece(self):
-		# Returns index, offset and block except when there's
-		# an unpack error. In the second case, It raises a TypeError
-		if not 'pieces' in self.artifacts: self.artifacts['pieces'] = list()
-		block_len = self.message_len - 9
-		total = block_len + 13
-		try:
-			index, offset = unpack('>II', self.response[5: 13])
-			data = self.response[13:total]
-			block_info = (index, offset, data)
-			self.artifacts['pieces'].append(block_info)
-		except UnpackError:
-			raise TypeError("Parser: Failed to extract piece")
-		finally:
-			self.response = self.response[total:]
-		
-		
-	def parse_bitfield(self):
-		# Since bitfield len is 1+X. X is length of bitfield
-		self.message_len -= 1
-		total = self.message_len + self.message_id
-		message = self.response[5:total]
-		self.response = self.response[total:]
-		self.artifacts.update({'bitfield': message})
-		
-		
-	def parse_handshake(self):
-		# Total is 68 bytes
-		total = 68
-		message = self.response[:total]
-		self.response = self.response[total:]
-		self.artifacts.update({'handshake': message})
-
-		
-
-if __name__ == "__main__":
-	...
+    def parse_keep_alive(self):
+        self.response = self.response[4:]
+        self.artifacts.update({'keep_alive': True})
+    
+    def parse_choke(self):
+        self.response = self.response[5:]
+        self.artifacts.update({'choke': True})
+        
+    def parse_unchoke(self):
+        self.response = self.response[5:]
+        self.artifacts.update({'unchoke': True})
+        
+    def parse_have(self):
+        if len(self.response) < 9: return
+        piece_index = unpack('>I', self.response[5:9])[0]
+        self.response = self.response[9:]
+        # Fix: Store multiple 'have' messages in a list instead of overwriting
+        if 'have' not in self.artifacts: self.artifacts['have'] = []
+        self.artifacts['have'].append(piece_index)
+    
+    def parse_piece(self):
+        block_len = self.message_len - 9
+        total = block_len + 13
+        if len(self.response) < total: return
+        
+        try:
+            index, offset = unpack('>II', self.response[5:13])
+            data = self.response[13:total]
+            if 'pieces' not in self.artifacts: self.artifacts['pieces'] = []
+            self.artifacts['pieces'].append((index, offset, data))
+        except UnpackError:
+            raise TypeError("Parser: Malformed piece")
+        finally:
+            self.response = self.response[total:]
+        
+    def parse_bitfield(self):
+        total = self.message_len + 4
+        if len(self.response) < total: return
+        message = self.response[5:total]
+        self.response = self.response[total:]
+        self.artifacts.update({'bitfield': message})
+        
+    def parse_handshake(self):
+        if len(self.response) < 68: return
+        message = self.response[:68]
+        self.response = self.response[68:]
+        self.artifacts.update({'handshake': message})
